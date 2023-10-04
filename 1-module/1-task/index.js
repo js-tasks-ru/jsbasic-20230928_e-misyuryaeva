21a0b65b1a449e0dee428372ec5c3442b757b713
evgeniya misyur <chudno2000@gmail.com>
2023-10-04 22:19:05 +0300

    добавлена функция факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,21 @@
 function factorial(n) {
-  // ваш код...
+  
+  let result;
+	
+	if(!n) {
+	
+	return result = 1;
+	
+	} else {
+	
+		result = n;
+		let i = n - 1;
+		
+		for(i; i > 1; i--) {
+        
+			result *= i;
+      
+		}
+	}
+    return result;
 }
