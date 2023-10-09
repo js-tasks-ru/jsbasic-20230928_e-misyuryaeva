6f95bb0d045781d2e459a84374d2d91758387548
evgeniya misyur <chudno2000@gmail.com>
2023-10-09 09:46:13 +0300

    добавлена функция суммирования зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;  
+  
+  for (let key in salaries) { 
+    
+    if (  (typeof(salaries[key]) == 'number') && !Number.isNaN(salaries[key]) && Number.isFinite(salaries[key]) ) {
+   
+     sum += salaries[key];
+	 
+     };
+   }   
+
+  return sum;
+  
 }
