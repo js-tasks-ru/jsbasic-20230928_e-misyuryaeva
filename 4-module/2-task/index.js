454cc13c212fa0cf1a90aa6092c29ad7d84b3603
evgeniya misyur <chudno2000@gmail.com>
2023-10-19 12:08:44 +0300

    добавлено выделение диагональной ячейки красным цветом

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,15 @@
 function makeDiagonalRed(table) {
+  for (let tr of table.rows) {
+  
+    let rowIndex = tr.rowIndex;
+    
+    for (let td of tr.cells) {
+    
+      let cellIndex = td.cellIndex;
+    
+      ( cellIndex == rowIndex) ? (td.style.backgroundColor = 'red'):(td.style.backgroundColor = '');
+    
+    };  
+  }  
   // ваш код...
 }
