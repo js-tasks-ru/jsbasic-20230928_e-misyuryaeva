4df2c5a41567d09d22b0c8a88e81e2b3704676c9
evgeniya misyur <chudno2000@gmail.com>
2023-10-20 11:44:49 +0300

    добавлена функция highlight (тесты не проходят)

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,37 @@
 function highlight(table) {
-  // ваш код...
+  
+ let rows = table.rows;
+ for (i = 1; i < rows.length; i++) {
+   
+   	if (table.rows[i].cells[1].innerHTML < 18) {
+	
+	  rows[i].style.textDecoration = 'line-through'; 
+    };
+	
+	  if (table.rows[i].cells[2].innerHTML == 'm') {
+	
+	    rows[i].className = 'male'; 
+	 
+	  } else if (table.rows[i].cells[2].innerHTML == 'f') {
+	
+	    rows[i].className = 'female'; 
+	 
+	  };
+	
+	let status = table.rows[i].cells[3].getAttribute('data-available')
+	     
+	 if (status == 'true') {
+	
+	   rows[i].className += ' available'; 
+	 
+	 } else if (status == 'false'){
+	
+	   rows[i].className += ' unavailable'; 
+	 
+	 } else if (status == null)	{
+	
+	   rows[i].hidden = true;
+	
+	 };	   
+ }
 }
