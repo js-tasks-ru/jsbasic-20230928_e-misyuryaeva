d121f9a3818bc50c35c0f77fe61b43736823e66d
evgeniya misyur <chudno2000@gmail.com>
2023-10-27 09:44:25 +0300

    добавлена карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+
+   let carouselElement = document.querySelector('.carousel__inner');
+   let buttonRight =  document.querySelector('.carousel__arrow_right');
+   let buttonLeft =  document.querySelector('.carousel__arrow_left');
+   buttonLeft.style.display = 'none';   
+   let widthSlide = document.querySelector('.carousel__slide').offsetWidth;
+   let i = 0;
+   
+   buttonRight.addEventListener('click', ()=>{
+		 i += 1;	
+		 carouselElement.style.transform = 'translateX('+(-widthSlide*i)+'px)';	
+     displayButtons();
+   });
+
+   buttonLeft.addEventListener('click', ()=>{
+		 i -= 1;	
+		 carouselElement.style.transform = 'translateX('+(-widthSlide*i)+'px)';
+		 displayButtons();     
+   });
+   
+    displayButtons = () => {
+	
+     if(i == 0){         
+ 		    buttonLeft.style.display =	'none';			  
+      } else {          
+		    buttonLeft.style.display =	'';	  
+		 };
+		
+	   if(i == 3){	         
+ 		    buttonRight.style.display =	'none';			  
+      } else {		          
+		    buttonRight.style.display =	'';		  
+		 };
+    };
+	
 }
