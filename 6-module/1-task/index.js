399cc7aa6453b5e1210d1c5d4905ce28d526cc70
evgeniya misyur <chudno2000@gmail.com>
2023-11-09 17:04:47 +0300

    таблица с удаляемыми строками и промис

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,38 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
-  }
+  constructor(rows) {  
+		
+		this.render(rows);
+		this.addEventListener();	
+	
+    }
+	
+	render(rows){
+		let table = document.createElement( 'table' );
+		table.insertAdjacentHTML( 'afterbegin', 
+		  '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead>' );
+		let tbody = document.createElement(  'tbody' );
+		table.append(tbody); 			
+			rows.forEach(function(item, index, array) {
+			  tbody.insertAdjacentHTML('beforeend', 
+			  '<tr><td>' + item.name + '</td><td>' + item.age + '</td><td>' + item.salary + '</td><td>' + item.city + '</td><td><button>x</button></td></tr>');
+			});	
+		this.table =  table;	
+	}
+	
+   addEventListener(){
+    let buttons = this.table.querySelectorAll('button');
+	  buttons.forEach( function removeTr(button) {		
+		button.addEventListener('click', (event) => {	
+		   let btn = event.currentTarget;
+		   let tr = btn.closest('tr');
+		   tr.remove();	
+		  });
+	  });
+   }
+     
+   get elem() {
+     return this.table;
+    } 
 }
--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,6 @@
 export default function promiseClick(button) {
+  return new Promise(function(resolve) {
+    button.addEventListener('click', (event) => { resolve(event) }, { once: true });
+  }); 
   // ваш код...
 }
